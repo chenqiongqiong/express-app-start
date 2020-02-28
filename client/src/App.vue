01458e96880903686a80e7886aac8554128fcbd5
chenqiong <619591477@qq.com>
2020-02-28 14:54:49 +0800

    feat:  init

---
+++ client/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" @click="request">
+    <HelloWorld msg="Welcome to Your V123ue.js App"/>
+    <router-link to="/foo">Go to Foo</router-link>
+    <router-link to="/bar">Go to Bar</router-link>
+    <div>below is router view</div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+const axios = require('axios');
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    request() {
+      axios.get('/api/sql').then(() => {
+        console.log('success');
+      });
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
